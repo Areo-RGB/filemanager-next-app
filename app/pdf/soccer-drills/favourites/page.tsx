805a8f27ffa947706ce39d3efa3a3842a10db0dc
Areo-RGB <paziske@gmail.com>
2026-02-20 21:02:44 +0100

    123

--- app/pdf/soccer-drills/favourites/page.tsx
+++ app/pdf/soccer-drills/favourites/page.tsx
@@ -1,19 +1,16 @@
 import { FloatingDockDemo } from "@/components/floating-dock-demo";
 import { TopRightControls } from "@/components/top-right-controls";
-import { TeamPdfList } from "@/components/team-pdf-list";
+import { TeamFavouritesList } from "@/components/team-favourites-list";
 
 export default function SoccerDrillsFavouritesPage() {
     return (
         <>
             <TopRightControls />
             <main className="flex min-h-screen justify-center px-4 pt-24 pb-16">
-                <TeamPdfList
+                <TeamFavouritesList
                     team="soccer-drills"
-                    title="Soccer Drills"
-                    backHref="/pdf"
-                    initialPdfs={[]}
-                    initialError={null}
-                    showFavouritesOnly={true}
+                    title="Soccer Drills Favourites"
+                    backHref="/pdf/soccer-drills"
                 />
             </main>
             <FloatingDockDemo />
